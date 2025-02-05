71ec1acf1f6f7c27e9fc99111bb31ac52c78dd9f
momo631 <631553794@qq.com>
2025-02-05 20:30:58 +0800

    3

--- src/main/java/com/momosensei/momotinker/Momotinker.java
+++ src/main/java/com/momosensei/momotinker/Momotinker.java
@@ -1,30 +1,17 @@
 package com.momosensei.momotinker;
 
-
-import com.momosensei.momotinker.entity.MomoDamageTypeProvider;
-import com.momosensei.momotinker.entity.MomoDamageTypeTagProvider;
 import com.momosensei.momotinker.event.LivingEvents;
+import com.momosensei.momotinker.network.Channel;
 import com.momosensei.momotinker.register.*;
-import net.minecraft.core.HolderLookup;
-import net.minecraft.core.RegistrySetBuilder;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.jetbrains.annotations.NotNull;
 import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
-import slimeknights.tconstruct.library.utils.Util;
 
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 
 @Mod(Momotinker.MOD_ID)
 @Mod.EventBusSubscriber(
@@ -34,8 +21,10 @@ import java.util.concurrent.CompletableFuture;
 public class Momotinker {
     public static final String MOD_ID = "momotinker"; //是你的模组名，需要英文
     public Momotinker(FMLJavaModLoadingContext context) {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        //注册表之类的东西
+        //如果你新稿了别的注册表记得这边填一下
         IEventBus eventBus = context.getModEventBus();
+        CREATIVE_MODE_TABS.register(eventBus);
         MinecraftForge.EVENT_BUS.register(new LivingEvents());
         MinecraftForge.EVENT_BUS.register(this);
         MomotinkerItem.ITEMS.register(eventBus);
@@ -44,22 +33,11 @@ public class Momotinker {
         MomotinkerBlock.BLOCK.register(eventBus);
         MomotinkerEffects.EFFECT.register(eventBus);
         MomotinkerEntities.ENTITIES.register(eventBus);
-        bus.register(new MomotinkerTools());
-        MomotinkerTables.initRegisters();
+        MomotinkerLootModifiers.register(eventBus);
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, MomotinkerConfig.spec);
+    }
 
-    }
-    @SubscribeEvent
-    static void gatherData(final GatherDataEvent event) {
-        DataGenerator generator = event.getGenerator();
-        PackOutput packOutput = generator.getPackOutput();
-        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
-        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-        RegistrySetBuilder registrySetBuilder = new RegistrySetBuilder();
-        MomoDamageTypeProvider.register(registrySetBuilder);
-        boolean server = event.includeServer();
-        DatapackBuiltinEntriesProvider datapackRegistryProvider = new DatapackBuiltinEntriesProvider(packOutput, lookupProvider, registrySetBuilder, Set.of(MOD_ID));
-        generator.addProvider(server, new MomoDamageTypeTagProvider(packOutput, datapackRegistryProvider.getRegistryProvider(), existingFileHelper));
-    }
     //Resourcelocation
     public static ResourceLocation getResource(String id) {
         return new ResourceLocation("momotinker", id);
@@ -67,14 +45,15 @@ public class Momotinker {
     public static <T> TinkerDataCapability.TinkerDataKey<T> createKey(String name) {
         return TinkerDataCapability.TinkerDataKey.of(getResource(name));
     }
-    public static MutableComponent makeTranslation(String base, String name) {
-        return Component.translatable(makeTranslationKey(base, name));
-    }
-    public static String makeTranslationKey(String base, String name) {
-        return Util.makeTranslationKey(base, getResource(name));
+    private void commonSetup(FMLCommonSetupEvent event) {
+        Channel.init();
     }
     //生成键名用的
     public static String makeDescriptionId(String type, String name) {
         return type + ".momotinker." + name;
+    }
+
+    public static ResourceLocation id(@NotNull String path) {
+        return new ResourceLocation(Momotinker.MOD_ID, path);
     }
 }
