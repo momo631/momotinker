26e0bc3a8cee0408c4020b4378fb20e33409bf61
momo631 <631553794@qq.com>
2025-02-05 20:30:58 +0800

    3

--- src/main/java/com/momosensei/momotinker/Momotinker.java
+++ src/main/java/com/momosensei/momotinker/Momotinker.java
@@ -1,20 +1,17 @@
 package com.momosensei.momotinker;
 
-
 import com.momosensei.momotinker.event.LivingEvents;
+import com.momosensei.momotinker.network.Channel;
 import com.momosensei.momotinker.register.*;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.jetbrains.annotations.NotNull;
 import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 
-import static slimeknights.tconstruct.TConstruct.makeTranslationKey;
 
 @Mod(Momotinker.MOD_ID)
 @Mod.EventBusSubscriber(
@@ -24,8 +21,10 @@ import static slimeknights.tconstruct.TConstruct.makeTranslationKey;
 public class Momotinker {
     public static final String MOD_ID = "momotinker"; //是你的模组名，需要英文
     public Momotinker(FMLJavaModLoadingContext context) {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        //注册表之类的东西
+        //如果你新稿了别的注册表记得这边填一下
         IEventBus eventBus = context.getModEventBus();
+        CREATIVE_MODE_TABS.register(eventBus);
         MinecraftForge.EVENT_BUS.register(new LivingEvents());
         MinecraftForge.EVENT_BUS.register(this);
         MomotinkerItem.ITEMS.register(eventBus);
@@ -35,11 +34,10 @@ public class Momotinker {
         MomotinkerEffects.EFFECT.register(eventBus);
         MomotinkerEntities.ENTITIES.register(eventBus);
         MomotinkerLootModifiers.register(eventBus);
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, MomotinkerConfig.spec);
-        bus.register(new MomotinkerTools());
-        MomotinkerTables.initRegisters();
-
     }
+
     //Resourcelocation
     public static ResourceLocation getResource(String id) {
         return new ResourceLocation("momotinker", id);
@@ -47,12 +45,15 @@ public class Momotinker {
     public static <T> TinkerDataCapability.TinkerDataKey<T> createKey(String name) {
         return TinkerDataCapability.TinkerDataKey.of(getResource(name));
     }
+    private void commonSetup(FMLCommonSetupEvent event) {
+        Channel.init();
+    }
     //生成键名用的
     public static String makeDescriptionId(String type, String name) {
         return type + ".momotinker." + name;
     }
 
-    public static MutableComponent makeTranslation(String base, String name) {
-        return Component.translatable(makeTranslationKey(base, name));
+    public static ResourceLocation id(@NotNull String path) {
+        return new ResourceLocation(Momotinker.MOD_ID, path);
     }
 }
