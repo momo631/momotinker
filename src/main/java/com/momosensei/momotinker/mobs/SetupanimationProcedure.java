76d1c0ec0fa21e1374beb80832a135efbef9788b
momo631 <631553794@qq.com>
2025-02-11 22:15:07 +0800

    1

--- src/main/java/com/momosensei/momotinker/mobs/SetupanimationProcedure.java
+++ src/main/java/com/momosensei/momotinker/mobs/SetupanimationProcedure.java
@@ -16,14 +16,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.network.NetworkEvent;
 
-import javax.annotation.Nullable;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 @Mod.EventBusSubscriber(modid = "momotinker", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -33,7 +32,7 @@ public class SetupanimationProcedure {
 		PlayerAnimationFactory.ANIMATION_DATA_FACTORY.registerFactory(new ResourceLocation("momotinker", "player_animation"), 1000, SetupanimationProcedure::registerPlayerAnimations);
 	}
 
-	private static IAnimation registerPlayerAnimations(net.minecraft.client.player.AbstractClientPlayer player) {
+	private static IAnimation registerPlayerAnimations(AbstractClientPlayer player) {
 		return new ModifierLayer<>();
 	}
 
@@ -64,7 +63,7 @@ public class SetupanimationProcedure {
 		public static void handler(CModAnimationMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
 			NetworkEvent.Context context = contextSupplier.get();
 			context.enqueueWork(() -> {
-				Level level = context.getSender().level;
+				Level level = Objects.requireNonNull(context.getSender()).level;
 				if (level.getEntity(message.target) != null) {
 					ServerPlayer player = (ServerPlayer) level.getEntity(message.target);
 					setAnimationClientside(player, message.animation.getString(), message.override);
@@ -82,17 +81,10 @@ public class SetupanimationProcedure {
 	@OnlyIn(Dist.CLIENT)
 	public static void setAnimationClientside(Player player, String anim, boolean override) {
 		if (player instanceof AbstractClientPlayer player_) {
-			var animation = (ModifierLayer<IAnimation>) PlayerAnimationAccess.getPlayerAssociatedData(player_).get(new ResourceLocation("momotinker", "player_animation"));
+			var animation = (ModifierLayer<IAnimation>) PlayerAnimationAccess.getPlayerAssociatedData(player_).get(new ResourceLocation("momotinker", "animations"));
             if (animation != null &&override) {
-                animation.setAnimation(new KeyframeAnimationPlayer(PlayerAnimationRegistry.getAnimation(new ResourceLocation("momotinker", anim))));
+                animation.setAnimation(new KeyframeAnimationPlayer(Objects.requireNonNull(PlayerAnimationRegistry.getAnimation(new ResourceLocation("momotinker", anim)))));
             }
         }
-	}
-
-	public static void execute() {
-		execute(null);
-	}
-
-	private static void execute(@Nullable Event event) {
 	}
 }
