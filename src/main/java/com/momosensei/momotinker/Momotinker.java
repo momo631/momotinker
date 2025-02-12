cdc97a9dd17c36de777466e1f2f57e34bac15ed5
momo631 <631553794@qq.com>
2025-02-12 22:40:56 +0800

    3

--- src/main/java/com/momosensei/momotinker/Momotinker.java
+++ src/main/java/com/momosensei/momotinker/Momotinker.java
@@ -9,6 +9,8 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
@@ -19,11 +21,10 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
+import slimeknights.tconstruct.library.utils.Util;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-
-import static com.momosensei.momotinker.register.MomotinkerTab.CREATIVE_MODE_TABS;
 
 @Mod(Momotinker.MOD_ID)
 @Mod.EventBusSubscriber(
@@ -33,10 +34,8 @@ import static com.momosensei.momotinker.register.MomotinkerTab.CREATIVE_MODE_TAB
 public class Momotinker {
     public static final String MOD_ID = "momotinker"; //是你的模组名，需要英文
     public Momotinker(FMLJavaModLoadingContext context) {
-        //注册表之类的东西
-        //如果你新稿了别的注册表记得这边填一下
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         IEventBus eventBus = context.getModEventBus();
-        CREATIVE_MODE_TABS.register(eventBus);
         MinecraftForge.EVENT_BUS.register(new LivingEvents());
         MinecraftForge.EVENT_BUS.register(this);
         MomotinkerItem.ITEMS.register(eventBus);
@@ -45,6 +44,8 @@ public class Momotinker {
         MomotinkerBlock.BLOCK.register(eventBus);
         MomotinkerEffects.EFFECT.register(eventBus);
         MomotinkerEntities.ENTITIES.register(eventBus);
+        bus.register(new MomotinkerTools());
+        MomotinkerTables.initRegisters();
 
     }
     @SubscribeEvent
@@ -65,6 +66,12 @@ public class Momotinker {
     }
     public static <T> TinkerDataCapability.TinkerDataKey<T> createKey(String name) {
         return TinkerDataCapability.TinkerDataKey.of(getResource(name));
+    }
+    public static MutableComponent makeTranslation(String base, String name) {
+        return Component.translatable(makeTranslationKey(base, name));
+    }
+    public static String makeTranslationKey(String base, String name) {
+        return Util.makeTranslationKey(base, getResource(name));
     }
     //生成键名用的
     public static String makeDescriptionId(String type, String name) {
