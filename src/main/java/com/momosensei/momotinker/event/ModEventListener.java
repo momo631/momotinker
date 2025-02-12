6e22807eb878b1547ea853eaf34346c47f7a2727
momo631 <631553794@qq.com>
2025-02-12 22:40:56 +0800

    3

--- src/main/java/com/momosensei/momotinker/event/ModEventListener.java
+++ src/main/java/com/momosensei/momotinker/event/ModEventListener.java
@@ -1,11 +1,9 @@
 package com.momosensei.momotinker.event;
 
-
-import com.momosensei.momotinker.capability.ender.EnderProvider;
-import com.momosensei.momotinker.gui.overlay.CensoredHUD;
+import com.momosensei.momotinker.capability.healpercentage.EnderProvider;
 import com.momosensei.momotinker.gui.overlay.EnderOverlay;
-import com.momosensei.momotinker.gui.overlay.TriggerBladeHUD;
 import com.momosensei.momotinker.key.key;
+import com.momosensei.momotinker.mobs.TriggerBladeHUD;
 import com.momosensei.momotinker.network.Channel;
 import com.momosensei.momotinker.register.MomotinkerEntities;
 import com.momosensei.momotinker.renderer.triggerSlashRenderer;
@@ -32,8 +30,8 @@ public class ModEventListener {
     public static void registerOverlay(RegisterGuiOverlaysEvent event) {
         if (FMLEnvironment.dist == Dist.CLIENT) {
             event.registerAbove(VanillaGuiOverlay.HOTBAR.id(), "ender", EnderOverlay.ENDER);
-            event.registerAboveAll( "censored_hud", CensoredHUD.CENSORED);
-            event.registerAboveAll( "trigger_blade_hud", TriggerBladeHUD.TRIGGER_BLADE_HUD);
+            event.registerAboveAll( "bbb", CensoredHUD.CENSORED);
+            event.registerAboveAll( "trigger_blade", TriggerBladeHUD.TRIGGER_BLADE);
         }
     }
     @SubscribeEvent
