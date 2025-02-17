a52ae1e55947404a119f1cba719540c54bec23cc
momo631 <631553794@qq.com>
2025-02-17 12:54:12 +0800

    3

--- src/main/java/com/momosensei/momotinker/event/ModEvent.java
+++ src/main/java/com/momosensei/momotinker/event/ModEvent.java
@@ -1,6 +1,5 @@
 package com.momosensei.momotinker.event;
 
-import com.momosensei.momotinker.register.MomotinkerConfig;
 import com.momosensei.momotinker.register.MomotinkerItem;
 import com.momosensei.momotinker.register.MomotinkerModifiers;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
@@ -33,15 +32,12 @@ public class ModEvent {
     }
     @SubscribeEvent
     public static void addCustomTrades(VillagerTradesEvent event) {
-        boolean config = MomotinkerConfig.greedy_contract.get();
-        if (config) {
-            if (event.getType() != null) {
-                Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
-                ItemStack a = new ItemStack(MomotinkerItem.greedy_contract.get());
-                int villagerLevel = 5;
-                trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
-                        new ItemStack(Items.EMERALD_BLOCK, 16), a, 1, 0, 0.1f));
-            }
+        if (event.getType() != null) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack a = new ItemStack(MomotinkerItem.greedy_contract.get());
+            int villagerLevel = 5;
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD_BLOCK, 16), a, 1, 0, 0.1f));
         }
     }
 }
