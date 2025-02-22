112fea56ceffb5e899922e584ccf293464a00f9b
momo631 <631553794@qq.com>
2025-02-22 13:06:09 +0800

    3

--- src/main/java/com/momosensei/momotinker/event/ModEventListener.java
+++ src/main/java/com/momosensei/momotinker/event/ModEventListener.java
@@ -31,7 +31,7 @@ public class ModEventListener {
     public static void registerOverlay(RegisterGuiOverlaysEvent event) {
         if (FMLEnvironment.dist == Dist.CLIENT) {
             event.registerAbove(VanillaGuiOverlay.HOTBAR.id(), "ender", EnderOverlay.ENDER);
-            event.registerAboveAll( "censored", CensoredHUD.CENSORED);
+            event.registerAboveAll( "censored_hud", CensoredHUD.CENSORED);
             event.registerAboveAll( "trigger_blade_hud", TriggerBladeHUD.TRIGGER_BLADE_HUD);
         }
     }
