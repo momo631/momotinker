cacfd782faea9161a61200760413ea82bf9a1e23
momo631 <631553794@qq.com>
2025-02-23 21:19:54 +0800

    1

--- src/main/java/com/momosensei/momotinker/Momotinker.java
+++ src/main/java/com/momosensei/momotinker/Momotinker.java
@@ -4,15 +4,21 @@ import com.momosensei.momotinker.event.LivingEvents;
 import com.momosensei.momotinker.network.Channel;
 import com.momosensei.momotinker.register.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.jetbrains.annotations.NotNull;
+import slimeknights.tconstruct.library.client.model.TinkerItemProperties;
 import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
+
+import static com.momosensei.momotinker.register.MomotinkerItem.trigger_blade;
 
 
 @Mod(Momotinker.MOD_ID)
@@ -22,6 +28,7 @@ import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 
 public class Momotinker {
     public static final String MOD_ID = "momotinker"; //是你的模组名，需要英文
+
     public Momotinker() {
         //注册表之类的东西
         //如果你新稿了别的注册表记得这边填一下
@@ -43,12 +50,15 @@ public class Momotinker {
     public static ResourceLocation getResource(String id) {
         return new ResourceLocation("momotinker", id);
     }
+
     public static <T> TinkerDataCapability.TinkerDataKey<T> createKey(String name) {
         return TinkerDataCapability.TinkerDataKey.of(getResource(name));
     }
+
     private void commonSetup(FMLCommonSetupEvent event) {
         Channel.init();
     }
+
     //生成键名用的
     public static String makeDescriptionId(String type, String name) {
         return type + ".momotinker." + name;
@@ -56,5 +66,16 @@ public class Momotinker {
 
     public static ResourceLocation id(@NotNull String path) {
         return new ResourceLocation(Momotinker.MOD_ID, path);
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            event.enqueueWork(() -> {
+                TinkerItemProperties.registerBrokenProperty(trigger_blade.get());
+                TinkerItemProperties.registerToolProperties(trigger_blade.get());
+            });
+        }
     }
 }
