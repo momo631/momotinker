cdcb026d32f8dd2c096e92390afcb89d4e8c7a89
momo631 <631553794@qq.com>
2025-03-20 14:40:28 +0800

    1

--- src/main/java/com/momosensei/momotinker/key/key.java
+++ src/main/java/com/momosensei/momotinker/key/key.java
@@ -10,12 +10,12 @@ public class key {
     public key(){
     }
     public static class KeyBinding {
-        public static final String KEY_CATEGORY_EXAMPLE_MOD = "特殊技能";
-        public static final String KEY_TOOL_ENHANCEMENTE_MOD = "工具技能";
-        public static final String KEY_KEY = "墨工坊";
+        public static final String KEY_CATEGORY_EXAMPLE_MOD = "Category Example";
+        public static final String KEY_TOOL_ENHANCEMENT_MOD = "Tool Enhancement";
+        public static final String KEY_KEY = "momotinker";
 
         public static final KeyMapping KEY = new KeyMapping(KEY_CATEGORY_EXAMPLE_MOD, KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_X,KEY_KEY);
-        public static final KeyMapping KEYA = new KeyMapping(KEY_TOOL_ENHANCEMENTE_MOD, KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z,KEY_KEY);
+        public static final KeyMapping KEYA = new KeyMapping(KEY_TOOL_ENHANCEMENT_MOD, KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z,KEY_KEY);
 
     }
 }
