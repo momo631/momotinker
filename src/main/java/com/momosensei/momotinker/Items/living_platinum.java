d4468c6d4746575afa1d219bdb2fcb1e3030d662
momo631 <631553794@qq.com>
2025-04-26 17:43:54 +0800

    1

--- src/main/java/com/momosensei/momotinker/Items/living_platinum.java
+++ src/main/java/com/momosensei/momotinker/Items/living_platinum.java
@@ -22,6 +22,7 @@ public class living_platinum extends Item {
     @Override
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, List<Component> list, @NotNull TooltipFlag flag) {
         list.add(Component.translatable("momotinker.item.tooltip.living_platinum1"));
+        list.add(Component.translatable("momotinker.item.tooltip.living_platinum2"));
         super.appendHoverText(stack, level, list, flag);
     }
 
