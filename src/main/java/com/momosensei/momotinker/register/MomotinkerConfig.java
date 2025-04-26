c7c4a1281e84f00b80fbf15a8c7d7e8846649d0c
momo631 <631553794@qq.com>
2025-04-26 17:44:51 +0800

    1

--- src/main/java/com/momosensei/momotinker/register/MomotinkerConfig.java
+++ src/main/java/com/momosensei/momotinker/register/MomotinkerConfig.java
@@ -59,11 +59,11 @@ public class MomotinkerConfig {
     public static final ForgeConfigSpec.IntValue degenerate_limit=builder.comment("divine_punishment_spear_limit")
             .defineInRange("divine_punishment_spear degenerate limit",100,0,Integer.MAX_VALUE);
     public static final ForgeConfigSpec.IntValue hadal_limit=builder.comment("entropy_burning_riding_spear_limit")
-            .defineInRange("entropy_burning_riding_spear hadal limit",200,0,Integer.MAX_VALUE);
+            .defineInRange("entropy_burning_riding_spear hadal limit",400,0,Integer.MAX_VALUE);
     public static final ForgeConfigSpec.IntValue stellarcore_limit=builder.comment("entropy_burning_sword_limit")
-            .defineInRange("entropy_burning_sword stellarcore limit",200,0,Integer.MAX_VALUE);
+            .defineInRange("entropy_burning_sword stellarcore limit",400,0,Integer.MAX_VALUE);
     public static final ForgeConfigSpec.IntValue crystallized_limit=builder.comment("entropy_burning_cannon_limit")
-            .defineInRange("entropy_burning_cannon crystallized limit",10,0,Integer.MAX_VALUE);
+            .defineInRange("entropy_burning_cannon crystallized limit",20,0,Integer.MAX_VALUE);
     public static final ForgeConfigSpec.IntValue liverization_limit=builder.comment("entropy_burning_cube_limit")
             .defineInRange("entropy_burning_cube liverization limit",100,0,Integer.MAX_VALUE);
 
