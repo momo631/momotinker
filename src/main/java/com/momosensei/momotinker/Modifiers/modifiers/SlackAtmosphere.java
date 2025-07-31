3df0aad3cdfe5a6da22fffb3d9e17181e6b82ee7
momo631 <631553794@qq.com>
2025-07-31 12:09:03 +0800

    1

--- src/main/java/com/momosensei/momotinker/Modifiers/modifiers/SlackAtmosphere.java
+++ src/main/java/com/momosensei/momotinker/Modifiers/modifiers/SlackAtmosphere.java
@@ -4,32 +4,34 @@ import com.momosensei.momotinker.Modifiers.momomodifier;
 import com.momosensei.momotinker.register.MomotinkerModifiers;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.MobEffectEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
-import slimeknights.tconstruct.library.tools.nbt.ModDataNBT;
+import slimeknights.tconstruct.library.tools.nbt.ToolStack;
 
 import javax.annotation.Nullable;
 import java.util.*;
-
-import static com.momosensei.momotinker.Momotinker.getResource;
 
 
 public class SlackAtmosphere extends momomodifier {
     public SlackAtmosphere() {
         MinecraftForge.EVENT_BUS.addListener(this::AddMobEffect);
         MinecraftForge.EVENT_BUS.addListener(this::livingattackevent);
+        MinecraftForge.EVENT_BUS.addListener(this::onlivingtickevent);
     }
     @Override
     public boolean isNoLevels() {
@@ -58,70 +60,94 @@ public class SlackAtmosphere extends momomodifier {
         }
         return requirementsError(modifier);
     }
+    private void onlivingtickevent(LivingEvent.LivingTickEvent event) {
+        var entity=event.getEntity();
+        String s = "slackatmosphere";
+        if (entity.level().isClientSide)return;
+        if (entity.tickCount%10!=0)return;
+        var target=getSlackTarget(entity);
+        if (target==null)return;
+//        if (target.distanceTo(entity)>25||!target.isAlive()){
+//            entity.getPersistentData().remove(s);
+//        }
+        int a = 0;
+        if (entity instanceof Player player) {
+            for (int i = 0; i < Inventory.INVENTORY_SIZE; i++){
+                ItemStack stack = player.getInventory().getItem(i);
+                ToolStack tool=ToolStack.from(stack);
+                if (tool.getModifierLevel(MomotinkerModifiers.slackatmosphere.getId())>0){
+                    a+=1;
+                }
+            }
+            if (getAllModifierlevel(player,MomotinkerModifiers.slackatmosphere.getId())>0){
+                a+=getAllModifierlevel(player,MomotinkerModifiers.slackatmosphere.getId());
+            }
+        }
+        if (target.distanceTo(entity)>25||a==0||!entity.isAlive()){
+            entity.getPersistentData().remove(s);
+        }
+    }
+    private static LivingEntity getSlackTarget(LivingEntity living){
+        var nbt=living.getPersistentData();
+        String s = "slackatmosphere";
+        if (living.level() instanceof ServerLevel serverLevel&&nbt.contains(s)){
+            var uuid=nbt.getUUID(s);
+            var entity=serverLevel.getEntity(uuid);
+            if (entity instanceof LivingEntity living1){
+                return living1;
+            }
+        }
+        return null;
+    }
+    private static void setSlackTarget(LivingEntity attacker,LivingEntity target){
+        //var data1=attacker.getPersistentData();
+        var data2=target.getPersistentData();
+        String s = "slackatmosphere";
+        //data1.putUUID(s,target.getUUID());
+        data2.putUUID(s,attacker.getUUID());
+    }
     @Override
     public void onInventoryTick(IToolStackView tool, ModifierEntry modifierEntry, Level level, LivingEntity entity, int index, boolean b, boolean b1, ItemStack itemStack) {
-        if (entity instanceof Player player && !player.level.isClientSide) {
-            List<LivingEntity> ls0 = player.level.getEntitiesOfClass(LivingEntity.class, player.getBoundingBox().inflate(25));
-            List<LivingEntity> ls1 = player.level.getEntitiesOfClass(LivingEntity.class, player.getBoundingBox().inflate(21));
+        if (entity instanceof Player player && !player.level().isClientSide) {
+            List<LivingEntity> ls0 = player.level().getEntitiesOfClass(LivingEntity.class, player.getBoundingBox().inflate(25));
             for (LivingEntity living0 : ls0) {
-                for (LivingEntity living1 : ls1) {
-                    if (living1!=null&&living1!=player){
-                        ModDataNBT data = ModDataNBT.readFromNBT(living1.getPersistentData());
-                        if (!data.getBoolean(getResource("slackatmosphere"))){
-                            data.putBoolean(getResource("slackatmosphere"),true);
-                        }
-                    }
-                }
-                if (living0!=null&&living0!=player&&!ls1.contains(living0)){
-                    ModDataNBT data = ModDataNBT.readFromNBT(living0.getPersistentData());{
-                        if (data.getBoolean(getResource("slackatmosphere"))){
-                            data.putBoolean(getResource("slackatmosphere"),false);
-                        }
-                    }
+                if (living0 != null&&getSlackTarget(living0)==null) {
+                   setSlackTarget(player,living0);
                 }
             }
         }
     }
     public void AddMobEffect(MobEffectEvent.Added event) {
         LivingEntity living = event.getEntity();
-        if (living != null&&!living.level.isClientSide) {
-            ModDataNBT data = ModDataNBT.readFromNBT(living.getPersistentData());
-            if (data.getBoolean(getResource("slackatmosphere"))) {
-                List<Player> ls0 = living.level.getEntitiesOfClass(Player.class, living.getBoundingBox().inflate(20));
-                for (Player player : ls0) {
-                    if (player != null && getAllModifierlevel(player, MomotinkerModifiers.slackatmosphere.getId()) > 0) {
-                        var instance = event.getEffectInstance();
-                        var effect = instance.getEffect();
-                        if (instance.getAmplifier() != -1) {
-                            if (!effect.isBeneficial()) {
-                                instance.amplifier *= 2;
-                            } else if (effect.isBeneficial()){
-                                instance.amplifier /= 2;
+        if (living != null&&!living.level().isClientSide&&getSlackTarget(living)!=null) {
+            var instance = event.getEffectInstance();
+            var effect = instance.getEffect();
+            if (instance.getAmplifier() != -1) {
+                if (!effect.isBeneficial()) {
+                    instance.amplifier *= 2;
+                } else if (effect.isBeneficial()) {
+                    int a = 0;
+                    if (living instanceof Player player) {
+                        for (int i = 0; i < Inventory.INVENTORY_SIZE; i++){
+                            ItemStack stack = player.getInventory().getItem(i);
+                            ToolStack tool=ToolStack.from(stack);
+                            if (tool.getModifierLevel(MomotinkerModifiers.slackatmosphere.getId())>0){
+                                a+=1;
                             }
                         }
-                        break;
-                    }
-                }
-            }
-        }
-        if (living instanceof Player player&&!player.level.isClientSide){
-            List<Player> ls0 = player.level.getEntitiesOfClass(Player.class, player.getBoundingBox().inflate(20));
-            for (Player player1 : ls0) {
-                if (player1 != null &&getAllModifierlevel(player1,MomotinkerModifiers.slackatmosphere.getId())>0) {
-                    var instance = event.getEffectInstance();
-                    var effect = instance.getEffect();
-                    if (instance.getAmplifier() != -1) {
-                        if (!effect.isBeneficial()) {
-                            instance.amplifier *= 2;
-                        } else if (effect.isBeneficial()&&player1!=player){
-                            instance.amplifier /= 2;
+                        if (getArmorModifierlevel(player,MomotinkerModifiers.slackatmosphere.getId())>0){
+                            a+=1;
                         }
                     }
-                    break;
+                    if (a!=0){
+                        return;
+                    }
+                    instance.amplifier /= 2;
                 }
             }
         }
     }
+
     private static final List<MobEffect> HARMFUL_EFFECTS = new ArrayList<>();
     private static final Random RANDOM = new Random();
     static {
