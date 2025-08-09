ef4464efaf0414e24054f899476406a865bba63d
momo631 <631553794@qq.com>
2025-08-09 23:15:56 +0800

    1

--- src/main/java/com/momosensei/momotinker/Modifiers/momomodifier.java
+++ src/main/java/com/momosensei/momotinker/Modifiers/momomodifier.java
@@ -21,10 +21,6 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.event.entity.living.LivingDamageEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import slimeknights.mantle.client.TooltipKey;
@@ -76,6 +72,9 @@ public abstract class momomodifier extends Modifier implements MeleeDamageModifi
         ModifyDamageModifierHook, ModifierRemovalHook, BlockBreakModifierHook, EntityInteractionModifierHook, ToolStatsModifierHook,
         ToolDamageModifierHook,ModifyDamageSourceModifierHook,  VolatileDataModifierHook, RequirementsModifierHook, ValidateModifierHook, RepairFactorModifierHook {
 
+    public momomodifier() {
+    }
+
     @Override
     protected void registerHooks(ModuleHookMap.Builder builder) {
         super.registerHooks(builder);
@@ -90,12 +89,6 @@ public abstract class momomodifier extends Modifier implements MeleeDamageModifi
 
     }
 
-    public momomodifier() {
-        MinecraftForge.EVENT_BUS.addListener(this::LivingHurtEvent);
-        MinecraftForge.EVENT_BUS.addListener(this::LivingAttackEvent);
-        MinecraftForge.EVENT_BUS.addListener(this::LivingDamageEvent);
-    }
-
     @Override
     public float getRepairFactor(IToolStackView tool, ModifierEntry entry, float factor) {
         return factor;
@@ -104,124 +97,74 @@ public abstract class momomodifier extends Modifier implements MeleeDamageModifi
     public Component requirementsError(ModifierEntry entry) {
         return null;
     }
-
     @Override
     public @NotNull List<ModifierEntry> displayModifiers(ModifierEntry entry) {
         return List.of();
     }
-
     @Override
     public Component validate(IToolStackView tool, ModifierEntry modifier) {
         return null;
     }
-
-
     @Override
     public int onDamageTool(IToolStackView tool, ModifierEntry modifier, int amount, @Nullable LivingEntity livingEntity) {
         return amount;
     }
-
-    public void addAttributes(IToolStackView tool, ModifierEntry modifier, EquipmentSlot slot, BiConsumer<Attribute, AttributeModifier> consumer) {
-    }
-
-    public void processLoot(IToolStackView tool, ModifierEntry modifier, List<ItemStack> list, LootContext context) {
-    }
-
     @Override
     public float getMeleeDamage(IToolStackView tool, ModifierEntry modifier, ToolAttackContext context, float baseDamage, float damage) {
         return this.onGetMeleeDamage(tool, modifier, context, baseDamage, damage);
     }
-
     @Override
     public void onDamageDealt(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, LivingEntity entity, DamageSource damageSource, float amount, boolean isDirectDamage) {
         this.modifierDamageDealt(tool, modifier, context, slotType, entity, damageSource, amount, isDirectDamage);
     }
-
-    public float onGetMeleeDamage(IToolStackView tool, ModifierEntry modifier, ToolAttackContext context, float baseDamage, float damage) {
-        return damage;
-    }
-
-    public void modifierDamageDealt(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, LivingEntity entity, DamageSource damageSource, float amount, boolean isDirectDamage) {
-    }
-
-    public boolean isNoLevels() {
-        return false;
-    }
-
+    @Override
     public @NotNull Component getDisplayName(int level) {
         return this.isNoLevels() ? super.getDisplayName() : super.getDisplayName(level);
     }
-
-    public Component getDisplayName(IToolStackView tool, ModifierEntry entry) {
-        return entry.getDisplayName();
-    }
-
     @Override
     public ItemStack findAmmo(IToolStackView tool, ModifierEntry modifiers, LivingEntity livingEntity, ItemStack itemStack, Predicate<ItemStack> predicate) {
         return this.modifierFindAmmo(tool, modifiers, livingEntity, itemStack, predicate);
     }
-
     @Override
     public void onProjectileLaunch(IToolStackView tool, ModifierEntry modifiers, LivingEntity livingEntity, Projectile projectile, @Nullable AbstractArrow abstractArrow, ModDataNBT ModDataNBT, boolean primary) {
         this.modifierOnProjectileLaunch(tool, modifiers, livingEntity, projectile, abstractArrow, ModDataNBT, primary);
     }
-
     @Override
     public boolean onProjectileHitEntity(ModifierNBT modifiers, ModDataNBT persistentData, ModifierEntry modifier, Projectile projectile, EntityHitResult hit, @javax.annotation.Nullable LivingEntity attacker, @javax.annotation.Nullable LivingEntity target) {
         return this.modifierOnProjectileHitEntity(modifiers, persistentData, modifier, projectile, hit, attacker, target);
     }
-
-    public ItemStack modifierFindAmmo(IToolStackView tool, ModifierEntry modifiers, LivingEntity livingEntity, ItemStack itemStack, Predicate<ItemStack> predicate) {
-        return itemStack;
-    }
-
-    public void modifierOnProjectileLaunch(IToolStackView tool, ModifierEntry modifiers, LivingEntity livingEntity, Projectile projectile, @Nullable AbstractArrow abstractArrow, ModDataNBT ModDataNBT, boolean primary) {
-    }
-
-    public boolean modifierOnProjectileHitEntity(ModifierNBT modifiers, ModDataNBT persistentData, ModifierEntry modifier, Projectile projectile, EntityHitResult hit, @javax.annotation.Nullable LivingEntity attacker, @javax.annotation.Nullable LivingEntity target) {
-        return false;
-    }
-
     @Override
     public void onEquip(IToolStackView tool, ModifierEntry modifier, EquipmentChangeContext context) {
         this.modifierOnEquip(tool, modifier, context);
     }
-
     @Override
     public void onUnequip(IToolStackView tool, ModifierEntry modifier, EquipmentChangeContext context) {
         this.modifierOnUnequip(tool, modifier, context);
     }
-
     @Override
     public void onInventoryTick(IToolStackView iToolStackView, ModifierEntry modifierEntry, Level level, LivingEntity entity, int index, boolean b, boolean b1, ItemStack itemStack) {
         this.modifierOnInventoryTick(iToolStackView, modifierEntry, level, entity, index, b, b1, itemStack);
     }
-
     @Override
     public void onAttacked(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
         this.modifierOnAttacked(tool, modifier, context, slotType, source, amount, isDirectDamage);
     }
-
     @Override
     public float modifyDamageTaken(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
         return this.modifierDamageTaken(tool, modifier, context, slotType, source, amount, isDirectDamage);
     }
-
     @Override
     public void afterBlockBreak(IToolStackView tool, ModifierEntry modifier, ToolHarvestContext context) {
         this.modifierAfterBlockBreak(tool, modifier, context);
     }
-
     @Override
     public InteractionResult beforeEntityUse(IToolStackView tool, ModifierEntry modifier, Player player, Entity target, InteractionHand hand, InteractionSource source) {
         return this.modifierBeforeEntityUse(tool, modifier, player, target, hand, source);
     }
-
     @Override
     public InteractionResult afterEntityUse(IToolStackView tool, ModifierEntry modifier, Player player, LivingEntity target, InteractionHand hand, InteractionSource source) {
         return this.modifierAfterEntityUse(tool, modifier, player, target, hand, source);
     }
-
     @Override
     public void addToolStats(IToolContext context, ModifierEntry modifier, ModifierStatsBuilder builder) {
         this.modifierAddToolStats(context, modifier, builder);
@@ -230,68 +173,67 @@ public abstract class momomodifier extends Modifier implements MeleeDamageModifi
     public void addVolatileData(IToolContext context, ModifierEntry modifier, ToolDataNBT volatileData) {
         this.modifieraddVolatileData(context, modifier, volatileData);
     }
-
-    private void modifieraddVolatileData(IToolContext context, ModifierEntry modifier, ModDataNBT modDataNBT) {
-    }
-
-    public void modifierOnEquip(IToolStackView tool, ModifierEntry modifier, EquipmentChangeContext context) {
-    }
-
-    public void modifierOnUnequip(IToolStackView tool, ModifierEntry modifier, EquipmentChangeContext context) {
-    }
-
-    public void modifierOnInventoryTick(IToolStackView tool, ModifierEntry modifier, Level level, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack itemStack) {
-    }
-
-    public void modifierOnAttacked(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
-    }
-
-    public float modifierDamageTaken(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
-        return amount;
-    }
-
-    public void modifierAfterBlockBreak(IToolStackView tool, ModifierEntry modifier, ToolHarvestContext context) {
-    }
-
-    public InteractionResult modifierBeforeEntityUse(IToolStackView tool, ModifierEntry modifier, Player player, Entity target, InteractionHand hand, InteractionSource source) {
-        return InteractionResult.PASS;
-    }
-
-    public InteractionResult modifierAfterEntityUse(IToolStackView tool, ModifierEntry modifier, Player player, LivingEntity target, InteractionHand hand, InteractionSource source) {
-        return InteractionResult.PASS;
-    }
-
-    public void modifierAddToolStats(IToolContext context, ModifierEntry modifier, ModifierStatsBuilder builder) {
-    }
-
     @Override
     public void addTooltip(IToolStackView tool, ModifierEntry modifierEntry, @Nullable Player player, List<Component> tooltip, TooltipKey tooltipKey, TooltipFlag tooltipFlag) {
-
     }
-
     @Nullable
     @Override
     public Component onRemoved(IToolStackView iToolStackView, Modifier modifier) {
         this.onModifierRemoved(iToolStackView);
         return null;
     }
-
-    public void onModifierRemoved(IToolStackView tool) {
-    }
-
-    public void LivingHurtEvent(LivingHurtEvent target) {
-    }
-
-    public void LivingAttackEvent(LivingAttackEvent target) {
-    }
-
-    public void LivingDamageEvent(LivingDamageEvent target) {
-    }
-
     @Override
     public boolean startInteract(IToolStackView tool, ModifierEntry modifier, Player player, EquipmentSlot slot, TooltipKey keyModifier) {
         return KeybindInteractModifierHook.super.startInteract(tool, modifier, player, slot, keyModifier);
     }
+
+
+    public void addAttributes(IToolStackView tool, ModifierEntry modifier, EquipmentSlot slot, BiConsumer<Attribute, AttributeModifier> consumer) {
+    }
+    public void processLoot(IToolStackView tool, ModifierEntry modifier, List<ItemStack> list, LootContext context) {
+    }
+    public float onGetMeleeDamage(IToolStackView tool, ModifierEntry modifier, ToolAttackContext context, float baseDamage, float damage) {
+        return damage;
+    }
+    public void modifierDamageDealt(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, LivingEntity entity, DamageSource damageSource, float amount, boolean isDirectDamage) {
+    }
+    public boolean isNoLevels() {
+        return false;
+    }
+    public ItemStack modifierFindAmmo(IToolStackView tool, ModifierEntry modifiers, LivingEntity livingEntity, ItemStack itemStack, Predicate<ItemStack> predicate) {
+        return itemStack;
+    }
+    public void modifierOnProjectileLaunch(IToolStackView tool, ModifierEntry modifiers, LivingEntity livingEntity, Projectile projectile, @Nullable AbstractArrow abstractArrow, ModDataNBT ModDataNBT, boolean primary) {
+    }
+    public boolean modifierOnProjectileHitEntity(ModifierNBT modifiers, ModDataNBT persistentData, ModifierEntry modifier, Projectile projectile, EntityHitResult hit, @javax.annotation.Nullable LivingEntity attacker, @javax.annotation.Nullable LivingEntity target) {
+        return false;
+    }
+    private void modifieraddVolatileData(IToolContext context, ModifierEntry modifier, ModDataNBT modDataNBT) {
+    }
+    public void modifierOnEquip(IToolStackView tool, ModifierEntry modifier, EquipmentChangeContext context) {
+    }
+    public void modifierOnUnequip(IToolStackView tool, ModifierEntry modifier, EquipmentChangeContext context) {
+    }
+    public void modifierOnInventoryTick(IToolStackView tool, ModifierEntry modifier, Level level, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack itemStack) {
+    }
+    public void modifierOnAttacked(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
+    }
+    public float modifierDamageTaken(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
+        return amount;
+    }
+    public void modifierAfterBlockBreak(IToolStackView tool, ModifierEntry modifier, ToolHarvestContext context) {
+    }
+    public InteractionResult modifierBeforeEntityUse(IToolStackView tool, ModifierEntry modifier, Player player, Entity target, InteractionHand hand, InteractionSource source) {
+        return InteractionResult.PASS;
+    }
+    public InteractionResult modifierAfterEntityUse(IToolStackView tool, ModifierEntry modifier, Player player, LivingEntity target, InteractionHand hand, InteractionSource source) {
+        return InteractionResult.PASS;
+    }
+    public void modifierAddToolStats(IToolContext context, ModifierEntry modifier, ModifierStatsBuilder builder) {
+    }
+    public void onModifierRemoved(IToolStackView tool) {
+    }
+
 
     public static int getAllModifierlevel(LivingEntity entity, ModifierId modifierId) {
         return ModifierUtil.getModifierLevel(entity.getItemBySlot(EquipmentSlot.MAINHAND), modifierId)
