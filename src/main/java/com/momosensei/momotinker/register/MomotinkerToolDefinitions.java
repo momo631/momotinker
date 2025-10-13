18964937e56db3aca47765723928f8b334efb181
momo631 <631553794@qq.com>
2025-10-13 23:31:49 +0800

    1

--- src/main/java/com/momosensei/momotinker/register/MomotinkerToolDefinitions.java
+++ src/main/java/com/momosensei/momotinker/register/MomotinkerToolDefinitions.java
@@ -3,15 +3,11 @@ package com.momosensei.momotinker.register;
 
 import com.momosensei.momotinker.Momotinker;
 import slimeknights.tconstruct.library.tools.definition.ToolDefinition;
-import slimeknights.tconstruct.library.tools.stat.FloatToolStat;
 import slimeknights.tconstruct.library.tools.stat.ToolStatId;
-import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
 public class MomotinkerToolDefinitions {
     public MomotinkerToolDefinitions() {
     }
-    public static final FloatToolStat SLASH_COLOR = ToolStats.register(new FloatToolStat(name("slash_color"), -3135232, 0.0F, 0.0F, 11));
-    public static final FloatToolStat SCALE = (FloatToolStat) ToolStats.register(new FloatToolStat(name("scale"), -3135232, 1.0F, 0.0F, Integer.MAX_VALUE));
 
     private static ToolStatId name(String name) {
         return new ToolStatId(Momotinker.MOD_ID, name);
