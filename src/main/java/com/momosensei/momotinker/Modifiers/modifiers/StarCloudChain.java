4a9ebabab7796473f834764e006a7a42b448469b
momo631 <631553794@qq.com>
2025-12-19 09:21:29 +0800

    1

--- src/main/java/com/momosensei/momotinker/Modifiers/modifiers/StarCloudChain.java
+++ src/main/java/com/momosensei/momotinker/Modifiers/modifiers/StarCloudChain.java
@@ -11,24 +11,25 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
 import java.util.List;
 
+import static com.momosensei.momotinker.Momotinker.getResource;
+
 public class StarCloudChain extends momomodifier {
     public StarCloudChain() {
-
     }
-
+    private static final String star_chain = getResource("star_chain").toString();
     @Override
     public void OnLivingHurt(LivingHurtEvent event) {
         Entity a = event.getEntity();
         Entity b = event.getSource().getEntity();
         if (b instanceof Player player&&a instanceof LivingEntity living){
             int c = getAllModifierlevel(player,MomotinkerModifiers.starcloudchain.getId());
-            if (c>0&&!living.getTags().contains("starchain")) {
-                living.addTag("starchain");
-            }
             if (c>0) {
+                if (!isStarChain(living)) {
+                    setStarChain(living);
+                }
                 List<LivingEntity> list = player.level.getEntitiesOfClass(LivingEntity.class, player.getBoundingBox().inflate(5 + 3 * c));
                 for (LivingEntity entity : list) {
-                    if (entity != null&&entity.getTags().contains("starchain")) {
+                    if (entity != null&&entity!=player&&isStarChain(entity)) {
                         entity.hurt(DamageSource.playerAttack(player),event.getAmount()*(0.2F+0.1F*c));
                     }
                 }
@@ -36,17 +37,25 @@ public class StarCloudChain extends momomodifier {
         }
         if (a instanceof Player player&&b instanceof LivingEntity living){
             int c = getAllModifierlevel(player,MomotinkerModifiers.starcloudchain.getId());
-            if (c>0&&!living.getTags().contains("starchain")) {
-                living.addTag("starchain");
-            }
             if (c>0) {
+                if (!isStarChain(living)) {
+                    setStarChain(living);
+                }
                 List<LivingEntity> list = player.level.getEntitiesOfClass(LivingEntity.class, player.getBoundingBox().inflate(5 + 3 * c));
                 for (LivingEntity entity : list) {
-                    if (entity != null&&entity.getTags().contains("starchain")) {
+                    if (entity != null&&entity!=player&&isStarChain(entity)) {
                         entity.hurt(DamageSource.playerAttack(player),event.getAmount()*(0.2F+0.1F*c));
                     }
                 }
             }
         }
+    }
+    private static boolean isStarChain(LivingEntity living){
+        var data=living.getPersistentData();
+        return data.getBoolean(star_chain);
+    }
+    private static void setStarChain(LivingEntity living){
+        var data =living.getPersistentData();
+        data.putBoolean(star_chain, true);
     }
 }
